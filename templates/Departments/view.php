98de7e6d798be7e0972f2f9dc74c0cd2fb44065a
Sonny <adduxe@hotmail.com>
2026-03-20 22:04:37 -0700

    departments: Baked controllers and views.

---
+++ templates/Departments/view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Department $department
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Department'), ['action' => 'edit', $department->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Department'), ['action' => 'delete', $department->id], ['confirm' => __('Are you sure you want to delete # {0}?', $department->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Departments'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Department'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="departments view content">
+            <h3><?= h($department->dept_code) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Dept Code') ?></th>
+                    <td><?= h($department->dept_code) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Description') ?></th>
+                    <td><?= h($department->description) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($department->id) ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
---
+++ tests/Fixture/DepartmentsFixture.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * DepartmentsFixture
+ */
+class DepartmentsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'dept_code' => 'Lorem ipsum d',
+                'description' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
